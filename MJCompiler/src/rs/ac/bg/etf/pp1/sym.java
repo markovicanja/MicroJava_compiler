a73b7c216f9d733e03ce4922f5cf086289b24f97
Anja <markovicanja98@gmail.com>
2020-12-24 20:02:46 +0100

    Leksicka analiza

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,65 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+
+	// Keywords
+	public static final int PROGRAM = 1;
+	public static final int BREAK = 2;
+	public static final int CLASS = 3;
+	public static final int ENUM = 4;
+	public static final int ELSE = 5;
+	public static final int CONST = 6;
+	public static final int IF = 7;
+	public static final int SWITCH = 8;
+	public static final int DO = 9;
+	public static final int WHILE = 10;
+	public static final int NEW = 11;
+	public static final int PRINT = 12;
+	public static final int READ = 13;
+	public static final int RETURN = 14;
+	public static final int VOID = 15;
+	public static final int EXTENDS = 16;
+	public static final int CONTINUE = 17;
+	public static final int CASE = 18;
+	
+	// Identifiers
+	public static final int IDENT = 19;
+	
+	// Constants
+	public static final int NUM_CONST = 20;
+	public static final int CHAR_CONST = 21;
+	public static final int BOOL_CONST = 22;
+	
+	// Operators
+	public static final int PLUS = 6;
+	public static final int MINUS = 7;
+	public static final int MUL = 7;
+	public static final int DIV = 7;
+	public static final int MOD = 7;
+	public static final int EQUAL = 7;
+	public static final int NOT_EQUAL = 8;
+	public static final int GREATER = 9;
+	public static final int GREATER_EQUAL = 10;
+	public static final int LESSER = 11;
+	public static final int LESSER_EQUAL = 12;
+	public static final int AND = 13;
+	public static final int OR = 13;
+	public static final int ASSIGNMENT = 13;
+	public static final int INC = 13;
+	public static final int DEC = 13;
+	public static final int SEMICOLON = 13;
+	public static final int COMMA = 13;
+	public static final int FULL_STOP = 13;
+	public static final int LPAREN = 13;
+	public static final int RPAREN = 13;
+	public static final int LSQUARE = 13;
+	public static final int RSQUARE = 13;
+	public static final int LBRACE = 13;
+	public static final int RBRACE = 13;
+	public static final int QUESTION_MARK = 13;
+	public static final int COLON = 13;
+	
+	public static final int EOF = 14;
+	
+}
+
